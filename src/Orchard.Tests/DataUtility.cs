443b49985893d457d42136a83c9e5ceecfbfb4d2
caoxiangkun <caoxiangkun@gmail.com>
2016-05-10 07:30:56 +0800

    Test

--- src/Orchard.Tests/DataUtility.cs
+++ src/Orchard.Tests/DataUtility.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using Orchard.Data;
 using Orchard.Data.Providers;
@@ -9,10 +11,21 @@ using Orchard.Environment.ShellBuilders.Models;
 namespace Orchard.Tests {
     public static class DataUtility {
         public static ISessionFactoryHolder CreateSessionFactory(string fileName, params Type[] types) {
+            var parameters = new SessionFactoryParameters {
+                Provider = "SqlServerCe",
+                DataFolder = Path.GetDirectoryName(fileName),
+                RecordDescriptors = types.Select(t => new RecordBlueprint { TableName = "Test_" + t.Name, Type = t }).ToList()
+            };
+            var provider = new SqlServerCompactDataServicesProvider(fileName);
+            DbConfiguration configuration = provider.BuildConfiguration();
+            DbConfiguration.SetConfiguration(configuration);
+            //Database.SetInitializer<DbContext>(null);
+            var contextOptions = provider.GetContextOptions(parameters);
+            // Uncomment to display SQL while running tests
+            // ((MsSqlCeConfiguration)persistenceConfigurer).ShowSql();
 
-            //var persistenceModel = AutoMap.Source(new Types(types))
-            //    .Alterations(alt => AddAlterations(alt, types))
-            //    .Conventions.AddFromAssemblyOf<DataModule>();
+            var session = new DbContext(contextOptions.ConnectionString, contextOptions.Model);
+
             var persistenceModel = AbstractDataServicesProvider.CreatePersistenceModel(types.Select(t => new RecordBlueprint { TableName = "Test_" + t.Name, Type = t }).ToList());
             var persistenceConfigurer = new SqlServerCompactDataServicesProvider(fileName).GetPersistenceConfigurer(true/*createDatabase*/);
             // Uncomment to display SQL while running tests
@@ -34,7 +47,6 @@ namespace Orchard.Tests {
 
         private static void AddAlterations(AutoMappingAlterationCollection alterations, IEnumerable<Type> types) {
             foreach (var assembly in types.Select(t => t.Assembly).Distinct()) {
-                alterations.Add(new AutoMappingOverrideAlteration(assembly));
                 alterations.AddFromAssembly(assembly);
             }
             alterations.AddFromAssemblyOf<DataModule>();
